2e1916e0031bb4d2d9b5189f5757ed50773fc26f
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2020-10-29 03:35:34 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+let prefix = "PREFIX"
+
+let token = "Super_SECRET_BOT_TOKEN 👀"
+
+let owners = ["OWNER_ID"]
+
+let OwnerName = "OWNER_NAME"
+
+let guild = ["GUILD_ID"]
+
+
+let ModerationCooldown = "MOD_COOLDOWN"
+let infoCooldown = "INFO_COOLDOWN"
+let funCooldown = "FUN_COOLDOWN"
+
+let BotName = "BOT_NAME"
+
+let log_channel = "LOG_CHANNEL_ID"
+
+let embedColor = "DEFAULT_EMBED_COLOR"
+
+module.exports = {
+    prefix,
+    token,
+    owners,
+    guild,
+    ModerationCooldown,
+    infoCooldown,
+    funCooldown,
+    BotName,
+    OwnerName,
+    log_channel,
+    embedColor
+}
